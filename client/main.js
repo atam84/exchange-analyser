8ea128d9ce69d6b486cfae7332990a61b951979f
atam84 <amine.tamdy@gmail.com>
2020-02-07 23:25:21 -0500

    001

--- client/main.js
+++ client/main.js
@@ -1,22 +1,27 @@
+import 'bootstrap';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { ReactiveDict } from 'meteor/reactive-dict'
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { Mongo } from 'meteor/mongo'
+import { _ } from 'underscore';
+
+_display = new ReactiveVar();
+_data = new ReactiveDict();
+_autoRefresh = new ReactiveDict();
+
+_debug = true;
+_app_name = 'eXchange-Analyser';
+_version = 'beta v0.4.3';
+let _intervalHandler = undefined;
+
+
+Collections = {};
+Collections.params    = new Mongo.Collection(null);
+Collections.exchanges = new Mongo.Collection(null);
+Collections.markets   = new Mongo.Collection(null);
+
+import '../lib/router.js';
 
 import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
--- server/main.js
+++ server/main.js
@@ -1,5 +1,19 @@
 import { Meteor } from 'meteor/meteor';
 
+const database = new MongoInternals.RemoteCollectionDriver('mongodb://localhost:27017/crypto');
+//let numberOfDocs = database.open('boxes').find().count();
+
+console.dir(database);
+
+db = database.mongo.db;
+collections = db.listCollections();
+
+collections.each(function(n, collection){
+  if(collection){
+    console.log( collection.name );
+  }
+});
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
